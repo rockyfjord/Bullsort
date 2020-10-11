2d128f91d092a93fb116057eddfdd3d08d3a1e62
Rockford <rjhipp@eagles.usi.edu>
2020-10-11 00:40:45 -0500

    Added StoreID to Main Activity

--- app/src/main/java/com/example/bullsort/MainActivity.java
+++ app/src/main/java/com/example/bullsort/MainActivity.java
@@ -3,18 +3,23 @@ package com.example.bullsort;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //test = findViewById(R.id.txtStoreId);
     }
 
 
