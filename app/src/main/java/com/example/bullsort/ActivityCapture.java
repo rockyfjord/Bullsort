f48f23a158ccc7d8493b9761e687b0c2935fc67d
Rockford <rjhipp@eagles.usi.edu>
2020-10-10 21:03:43 -0500

    2nd Activity screen

---
+++ app/src/main/java/com/example/bullsort/ActivityCapture.java
@@ -0,0 +1,34 @@
+package com.example.bullsort;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+public class ActivityCapture extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
+    /** Called when the user taps the FINISHED button */
+    public void gotoActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/bullsort/MainActivity.java
+++ app/src/main/java/com/example/bullsort/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.bullsort;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    /** Called when the user taps the Capture button */
+    public void gotoActivity(View view) {
+        Intent intent = new Intent(this, ActivityCapture.class);
+        startActivity(intent);
+
+    }
+
+
+
 }
