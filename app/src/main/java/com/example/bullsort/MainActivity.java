5cba1e4eea81720a70db7551b4087184b8f73b0d
Sean Dodson <71029417+SeanDodson@users.noreply.github.com>
2020-10-13 22:05:43 -0500

    Update MainActivity.java
    added code to operate camera

--- app/src/main/java/com/example/bullsort/MainActivity.java
+++ app/src/main/java/com/example/bullsort/MainActivity.java
@@ -14,22 +14,34 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+
+    /**opens the default android camera
+     * Called when the user taps the Capture button*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        //test = findViewById(R.id.txtStoreId);
+
+        Button btnCapture = (Button) findViewById((R.id.btnCapture));
+        imageView = (ImageView) findViewById(R.id.imageView2);
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 
-
-    /** Called when the user taps the Capture button */
-    public void gotoActivity(View view) {
-        Intent intent = new Intent(this, ActivityCapture.class);
-        startActivity(intent);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
 
     }
-
-
-
 }
